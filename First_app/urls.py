18e5ec72dbe50ade990d5eb58d013c806baafabf
Daud <daud13t@gmail.com>
2025-11-29 10:34:58 +0000

    latest structure

--- First_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpattern = [
-    path('',views.Home.as_view(),name="home")
-]
--- First_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-# Create your views here.
-
-class Home(generic.TemplateView):
-    template_name = "home.html"
